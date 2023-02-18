5937b6d3eaee2b0bb311a9c538d69ede57ee5573
Thomas Cannon <tcannon00@gmail.com>
2023-02-18 13:36:45 -0500

    Generate `Users::Registrations/SessionsControllers`, `:registerable`
    * Tees up setup for `Devise::Passkeys`

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :recoverable, :rememberable, :validatable
+  devise :registerable, :recoverable, :rememberable, :validatable
 
   has_many :passkeys
 end
