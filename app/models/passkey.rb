e3e7c8af4fda59fc516bb3162009e21e0972a4ca
Thomas Cannon <tcannon00@gmail.com>
2023-02-18 10:26:02 -0500

    Add uniqueness validations for `Passkey.external_id/public_key/label`

--- app/models/passkey.rb
+++ app/models/passkey.rb
@@ -1,3 +1,7 @@
 class Passkey < ApplicationRecord
   belongs_to :user
+
+  validate :label, uniqueness: { scope: :user}
+  validate :external_id, uniqueness: true
+  validate :public_key, uniqueness: true
 end
