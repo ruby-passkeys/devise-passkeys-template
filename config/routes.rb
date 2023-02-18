05a104d42bdaeea17761de243f0534cc5063c1e0
Thomas Cannon <tcannon00@gmail.com>
2023-02-18 15:47:29 -0500

    Add Basic Registration form markup & routes for RegistrationsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   default_url_options protocol: :https
 
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+
+  devise_scope :user do
+    post 'sign_up/new_challenge', to: 'users/registrations#new_challenge', as: :new_user_registration_challenge
+  end
 
   # Defines the root path route ("/")
   root "root#index"
