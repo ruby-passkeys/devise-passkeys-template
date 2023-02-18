66fd3f63116b815e190783c5209c023d7765ba98
Thomas Cannon <tcannon00@gmail.com>
2023-02-18 15:45:21 -0500

    Remove `:validatable` from User devise setup because it's password-based
    * Until devise has first-class support for passkeys, we need to remove
    	the `Validatable` module because it assumes passwords are used

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :registerable, :recoverable, :rememberable, :validatable
+  devise :registerable, :rememberable
 
   has_many :passkeys
 end
