7798be275a11c2510429d3dac0b4fc5d6628aaeb
Thomas Cannon <tcannon00@gmail.com>
2023-02-18 17:06:12 -0500

    Add `User::SessionsController` implementation
    * Defines the `set_relying_party_in_request_env` for this resource,
    	based on the `RelyingParty` concern

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,9 @@
 # frozen_string_literal: true
 
-class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+class Users::SessionsController < Devise::Passkeys::SessionsController
+  include RelyingParty
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def set_relying_party_in_request_env
+    request.env[relying_party_key] = relying_party
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
 
   devise_scope :user do
     post 'sign_up/new_challenge', to: 'users/registrations#new_challenge', as: :new_user_registration_challenge
+    post 'sign_in/new_challenge', to: 'users/sessions#new_challenge', as: :new_user_session_challenge
   end
 
   # Defines the root path route ("/")
